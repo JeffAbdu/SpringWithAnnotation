e5fec4b7faa2054e089dff765bd94dc1bdce6450
JeffAbdu <abdenourdjafri@gmail.com>
2017-04-29 19:51:19 -0400

    Autowiring Constructor
    - Within Meal.java, We don't need setters, but we need "@Autowired" on
    the top of a constructor that uses arguments:
    
    @Autowired
    public Meal(Fruit fruit, Dairy dairy, Grain grain, Meat meat, Vegetable
    veg) {
    super();
    this.fruit = fruit;
    this.dairy = dairy;
    this.grain = grain;
    this.meat = meat;
    this.veg = veg;
    }
    
    - Autowiring is by type(like in Autowiring Properties), mean if we
    declare two beans with the same type, then Spring will not know which
    one would use for constructing.
    
    - As in "Autowiring Properties", All beans must be created and
    <context:annotation-config/> is added to "appContext.xml" the XML
    configuration file.

--- src/main/java/food/Meal.java
+++ src/main/java/food/Meal.java
@@ -12,53 +12,62 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Meal {
 	
-	//@Autowired   // This is autowiring by type: means if we define two fruit beans in the appContext.xml, we will have issue. 
 	private Fruit fruit;
-	//@Autowired
 	private Dairy dairy;
-	//@Autowired
 	private Grain grain;
-	//@Autowired
 	private Meat meat;
-	//@Autowired
 	private Vegetable veg;
 	
-	
-	public Fruit getFruit() {
-		return fruit;
-	}
+	public Meal() {}
+
+	// For Autowiring Constructor, we need Constructor using arguments with @Autowired annotation:  
 	@Autowired
-	public void setFruit(Fruit fruit) {
+	public Meal(Fruit fruit, Dairy dairy, Grain grain, Meat meat, Vegetable veg) {
+		super();
 		this.fruit = fruit;
-	}
-	public Dairy getDairy() {
-		return dairy;
-	}
-	@Autowired
-	public void setDairy(Dairy dairy) {
 		this.dairy = dairy;
-	}
-	public Grain getGrain() {
-		return grain;
-	}
-	@Autowired
-	public void setGrain(Grain grain) {
 		this.grain = grain;
-	}
-	public Meat getMeat() {
-		return meat;
-	}
-	@Autowired
-	public void setMeat(Meat meat) {
 		this.meat = meat;
-	}
-	public Vegetable getVeg() {
-		return veg;
-	}
-	//@Autowired
-	public void setVeg(Vegetable veg) {
 		this.veg = veg;
 	}
+	
+	
+//	public Fruit getFruit() {
+//		return fruit;
+//	}
+//	@Autowired
+//	public void setFruit(Fruit fruit) {
+//		this.fruit = fruit;
+//	}
+//	public Dairy getDairy() {
+//		return dairy;
+//	}
+//	@Autowired
+//	public void setDairy(Dairy dairy) {
+//		this.dairy = dairy;
+//	}
+//	public Grain getGrain() {
+//		return grain;
+//	}
+//	@Autowired
+//	public void setGrain(Grain grain) {
+//		this.grain = grain;
+//	}
+//	public Meat getMeat() {
+//		return meat;
+//	}
+//	@Autowired
+//	public void setMeat(Meat meat) {
+//		this.meat = meat;
+//	}
+//	public Vegetable getVeg() {
+//		return veg;
+//	}
+//	@Autowired
+//	public void setVeg(Vegetable veg) {
+//		this.veg = veg;
+//	}
+
 
 	public String whatsInThisMeal(){
 		
